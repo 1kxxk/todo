c881224193bcaf478eaecd2441ccf7a592ca0903
kwakhana <you@example.com>
2025-01-28 15:54:37 +0900

    CREATE 구현

--- todo-project/src/components/TodoList.jsx
+++ todo-project/src/components/TodoList.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const TodoList = () => {
-    const todos = [
+    const TODOS = [
         { id: 1, text: "Buy milk" },
         { id: 2, text: "Clean the house" },
         { id: 3, text: "Go for a run" },
@@ -13,14 +13,30 @@ const TodoList = () => {
         { id: 9, text: "Do laundry" },
         { id: 10, text: "Write code" },
       ];
+      const [todos, setTodos] = useState(TODOS)
+      const [text, setText] = useState('')
+      const handleSubmit = (e) => {
+        e.preventDefault()
+        if (!text.trim()) {
+          return;
+        }
+        setTodos([{id:crypto.randomUUID, text:text}, ...todos])
+        setText('')
+      }
   return (
-    <ul>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={text} onChange={(e)=> setText(e.target.value)}/>
+        <button>제출하기</button>
+      </form>
+      <ul>
         {todos.map((todo)=>(
         <li key={todo.id}>
             {todo.text}
         </li>
     ))}
     </ul>
+    </div>
   )
 }
 
