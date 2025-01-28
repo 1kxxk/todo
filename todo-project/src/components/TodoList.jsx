85d15febead90c3a5b230d6c07e268eaa85e1805
kwakhana <you@example.com>
2025-01-28 19:05:05 +0900

    Update 구현

--- todo-project/src/components/TodoList.jsx
+++ todo-project/src/components/TodoList.jsx
@@ -2,16 +2,16 @@ import React, { useState } from 'react'
 
 const TodoList = () => {
     const TODOS = [
-        { id: 1, text: "Buy milk" },
-        { id: 2, text: "Clean the house" },
-        { id: 3, text: "Go for a run" },
-        { id: 4, text: "Finish homework" },
-        { id: 5, text: "Call mom" },
-        { id: 6, text: "Buy groceries" },
-        { id: 7, text: "Walk the dog" },
-        { id: 8, text: "Read a book" },
-        { id: 9, text: "Do laundry" },
-        { id: 10, text: "Write code" },
+      { id: 1, text: "Buy milk", completed: false },
+      { id: 2, text: "Clean the house", completed: false },
+      { id: 3, text: "Go for a run", completed: false },
+      { id: 4, text: "Finish homework", completed: false },
+      { id: 5, text: "Call mom", completed: false },
+      { id: 6, text: "Buy groceries", completed: false },
+      { id: 7, text: "Walk the dog", completed: false },
+      { id: 8, text: "Read a book", completed: false },
+      { id: 9, text: "Do laundry", completed: false },
+      { id: 10, text: "Write code", completed: false },
       ];
       const [todos, setTodos] = useState(TODOS)
       const [text, setText] = useState('')
@@ -20,9 +20,15 @@ const TodoList = () => {
         if (!text.trim()) {
           return;
         }
-        setTodos([{id:crypto.randomUUID, text:text}, ...todos])
+        setTodos([{id:crypto.randomUUID(), text:text}, ...todos])
         setText('')
       }
+      const handleCompleted = (id) => {
+        const updatedTodos = todos.map((todo)=>{
+          return id === todo.id ? {...todo, completed: !todo.completed} : todo
+        })
+        setTodos(updatedTodos)
+      };
   return (
     <div>
       <form onSubmit={handleSubmit}>
@@ -31,8 +37,13 @@ const TodoList = () => {
       </form>
       <ul>
         {todos.map((todo)=>(
-        <li key={todo.id}>
+        <li key={todo.id} style={{
+          textDecoration: todo.completed ? 'line-through': 'none'
+        }}>
             {todo.text}
+            <button onClick={()=>handleCompleted(todo.id)}>
+              {todo.completed?'취소하기':'완료하기'}
+            </button>
         </li>
     ))}
     </ul>
